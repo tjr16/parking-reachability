76ea02aeaf50ed4c3575df144fe30c676b7cb883
Jiarui Tan (Jerry) <jerryaccount@126.com>
2021-12-17 10:07:14 +0100

    Data for competition

--- utils/patchMap.m
+++ utils/patchMap.m
@@ -6,6 +6,6 @@ for i = 1:N1
     obst = map_obst.obstacles{i};
     obsx = [obst{1:length(obst)}];
     obsy = [obst{length(obst)+1:2*length(obst)}];
-    patch(obsx,obsy,'black');
+    patch(obsx,obsy,'black', 'faceAlpha', 0.3);
 end
 
